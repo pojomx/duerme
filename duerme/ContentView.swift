70e925bf0eb23c5b70731f5e79438cd7b6033df8
Alan Milke <pojo@pojoclan.com>
2024-07-23 16:18:33 -0600

    Lo mismo que el anterior

--- duerme/ContentView.swift
+++ duerme/ContentView.swift
@@ -13,48 +13,9 @@ struct ContentView: View {
     @State var reproduciendo: Bool = false
     @State private var isEditing = false
     
-    class SoundElement: Identifiable {
-        var id: String
-        var player: AVAudioPlayer?
-        var volume: Float = 1.0
-        var name: String
-        var fileName: String
-        var fileExtension: String
-        
-        init(player: AVAudioPlayer? = nil, volume: Float, name: String, fileName: String, fileExtension: String) {
-            self.id = "\(fileName)\(fileExtension)"
-            self.player = player
-            self.volume = volume
-            self.name = name
-            self.fileName = fileName
-            self.fileExtension = fileExtension
-        }
-        
-        func setSound() {
-            guard let url = Bundle.main.url(forResource: self.fileName, withExtension: self.fileExtension) else { return }
-            
-            print("URL Establecida: \(url)")
-            
-            if let player = try? AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue) {
-                print("player inicializado")
-                player.numberOfLoops = -1 //Infinite
-                print("player en loop infinito")
-                player.volume = self.volume
-                print("player volumen en \(volume)")
-                player.play()
-                print("player reproduciendo")
-                self.player = player
-                print("player guardado")
-            } else {
-                print("Error: Error starting player.")
-                return
-            }
-        }
-    }
-    
     @State var soundList: [SoundElement] = [
-        SoundElement(player: AVAudioPlayer(), volume: 1.0, name: "Lluvia", fileName: "lluvia-pojoclan", fileExtension: "wav"),
-        //SoundElement(player: AVAudioPlayer(), volume: 1.0, name: "Truenos", fileName: "truenos", fileExtension: "wav"),
+        SoundElement(player: AVAudioPlayer(), volume: 1.0, name: "Lluvia Ligera", fileName: "lluvia-pojoclan", fileExtension: "wav"),
+        SoundElement(player: AVAudioPlayer(), volume: 0.0, name: "Lluvia Fuerte", fileName: "lluvia-fuerte", fileExtension: "wav"),
         //SoundElement(player: AVAudioPlayer(), volume: 1.0, name: "Viento", fileName: "viento", fileExtension: "wav")
     ]
             
--- duerme/Model/SoundElement.swift
+++ duerme/Model/SoundElement.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+import AVFAudio
+
+class SoundElement: Identifiable {
+    var id: String
+    var player: AVAudioPlayer?
+    var volume: Float = 1.0
+    var name: String
+    var fileName: String
+    var fileExtension: String
+    
+    init(player: AVAudioPlayer? = nil, volume: Float, name: String, fileName: String, fileExtension: String) {
+        self.id = "\(fileName)\(fileExtension)"
+        self.player = player
+        self.volume = volume
+        self.name = name
+        self.fileName = fileName
+        self.fileExtension = fileExtension
+    }
+    
+    func setSound() {
+        guard let url = Bundle.main.url(forResource: self.fileName, withExtension: self.fileExtension) else { return }
+        
+        print("URL Establecida: \(url)")
+        
+        if let player = try? AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue) {
+            player.numberOfLoops = -1 //Infinite
+            player.volume = self.volume
+        
+            player.play()
+            
+            self.player = player
+        } else {
+            print("Error: Error starting player.")
+            return
+        }
+    }
+}
